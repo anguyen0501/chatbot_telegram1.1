257137066c47d6b1653f63b4df8e4fe164d0c311
annguyen0501 <20h1120185@ut.edu.vn>
2022-12-26 19:38:58 +0700

    update (1)

--- Response.py
+++ Response.py
@@ -9,5 +9,7 @@ def sample_response(input):
     if user_mess in ("time", "time?"):
         now = "Hôm nay là: " + datetime.now().strftime("%d-%m-%y, %H:%M:%S")
         return str(now)
+    if user_mess in "bye":
+        return "Goodbye, see you later"
 
     return "Hỏi gì đó khác đi bro? Hỏi khó như này thì chịu rồi :<"
--- bot.py
+++ bot.py
@@ -1,12 +1,14 @@
 import Constants as keys
 import Response as R
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext, Filters
+from telegram import Update, ReplyKeyboardMarkup, bot
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext, Filters, CallbackQueryHandler
 import json
+import telegram
 from Method.News import News
 from googletrans import LANGUAGES, Translator
 import random
-
+import requests
+import ccxt
 
 print("Bot Starting....")
 
@@ -41,12 +43,104 @@ def file_command(update: Update, context: CallbackContext) -> None:
     context.bot.send_document(chat_id, file)
 
 
-def translate_command(update: Update, context: CallbackContext) -> None:
+def translate_command(update, context) -> None:
     message = update.message.text
-    dest_lang = random.choice(list(LANGUAGES.value()))
+    dest_lang = random.choice(list(LANGUAGES.values()))
     translator = Translator()
     translation = translator.translate(message, dest=dest_lang)
     context.bot.send_message(chat_id=update.effective_chat.id, text=translation.text)
+
+
+# Define the correct answer and the options
+correct_answer = "Paris"
+options = ["Paris", "Berlin", "Rome", "London"]
+
+
+# Define a function to handle the button press
+def button_press(update, context):
+    # Get the callback data
+    data = update.callback_query.data
+
+    # Extract the option number from the callback data
+    option = int(data.split("_")[1])
+
+    # Get the chat ID of the conversation
+    chat_id = update.effective_chat.id
+
+    # Check if the answer is correct
+    if options[option] == correct_answer:
+        context.bot.send_message(chat_id=chat_id, text="Correct guess!")
+    else:
+        context.bot.send_message(chat_id=chat_id, text="Wrong guess.")
+
+
+# Define a function to handle the /ask_question command
+def ask_question(update, context):
+    # Get the chat ID of the conversation
+    chat_id = update.effective_chat.id
+
+    # Generate a question
+    question = "What is the capital of France?"
+
+    # Create buttons for each option
+    buttons = []
+    for i, option in enumerate(options):
+        button = telegram.InlineKeyboardButton(option, callback_data=f"option_{i}")
+        buttons.append([button])
+
+    # Create a keyboard with the buttons
+    keyboard = telegram.InlineKeyboardMarkup(buttons)
+
+    # Send the question and keyboard to the user
+    context.bot.send_message(chat_id=chat_id, text=question, reply_markup=keyboard)
+
+
+def imgRandom_command(update, context):
+    # Make a request to the random image API
+    response = requests.get("https://source.unsplash.com/random")
+    # Send the image to the user
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=response.content)
+
+
+client = ccxt.binance()
+
+
+def price_command(update, context):
+    # Get the BTC/USDT market data from Binance
+    market = client.fetch_ticker("BTC/USDT")
+
+    # Extract the BTC price from the market data
+    price = market["last"]
+
+    # Send the BTC price to the user
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"Giá hiện tại của Bitcoin là ${price:.2f}.")
+
+
+def weather_command(update, context):
+    # Make a request to the OpenWeatherMap API for the weather in Ho Chi Minh City
+    response = requests.get(
+        "http://api.openweathermap.org/data/2.5/weather?q=Ho%20Chi%20Minh%20City&appid" + keys.OWM_KEY)
+    # Extract the weather data from the response
+    data = response.json()
+    temperature = data["main"]["temp"] - 273.15  # Convert from Kelvins to Celsius
+    weather = data["weather"][0]["main"]
+
+    # Send the weather data to the user
+    context.bot.send_message(chat_id=update.effective_chat.id,
+                             text=f"The weather in Ho Chi Minh City is currently {weather.lower()} with a temperature "
+                                  f"of {temperature:.1f}°C.")
+
+
+def export_history(update, context):
+    # Get the chat ID of the conversation
+    chat_id = update.effective_chat.id
+
+    # Get the history of messages in the conversation
+    history = update.messages.getHistory
+
+    # Save the history to a file
+    with open("history.json", "w") as f:
+        json.dump(history, f)
 
 
 def handle_message(update: Update, context: CallbackContext):
@@ -69,9 +163,16 @@ def main():
     dp.add_handler(CommandHandler("image", photo_command))
     dp.add_handler(CommandHandler("send", file_command))
     dp.add_handler(CommandHandler("translate", translate_command))
+    dp.add_handler(CommandHandler("imagee", imgRandom_command))
+    dp.add_handler(CommandHandler("price", price_command))
+    dp.add_handler(CommandHandler("weather", weather_command))
+    dp.add_handler(CommandHandler("save", export_history))
+    dp.add_handler(CallbackQueryHandler(button_press))
+    dp.add_handler(CommandHandler("ask_question", ask_question))
     dp.add_handler(MessageHandler(Filters.text, handle_message))
     dp.add_error_handler(error)
 
+    # Start the bot
     updater.start_polling()
     updater.idle()
 
